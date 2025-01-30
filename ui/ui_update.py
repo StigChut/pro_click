e2401bb97cb1693e625bf696d350a2aa49a06e03
StigChut <inflictionp@gmail.com>
2025-01-30 12:45:01 +0300

    \\\

--- ui/ui_update.py
+++ ui/ui_update.py
@@ -1,21 +1,9 @@
-import os
-
-from my_logger import logger, BASE_DIR
-<<<<<<< HEAD
+from my_logger import logger
 from update.auto_update import perform_fresh_update, perform_stable_update
 
 def run_fresh_update():
     try:
         perform_fresh_update()
-=======
-from update.fresh_update import check_fresh_update
-from update.stable_update import check_stable_update
-
-def run_fresh_update():
-
-    try:
-        check_fresh_update()
->>>>>>> debug
         print("Обновление завершено")
     except Exception as e:
         logger.exception(f"Error: {e}")
@@ -23,13 +11,8 @@ def run_fresh_update():
 
 
 def run_stable_update():
-
     try:
-<<<<<<< HEAD
         perform_stable_update()
-=======
-        check_stable_update()
->>>>>>> debug
         print("Обновление завершено")
     except Exception as e:
         logger.exception(f"Error: {e}")
